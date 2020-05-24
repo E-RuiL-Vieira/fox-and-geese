64fad38c2db08e7880f652cbc2f88c484f26bd0c
Rui Emanuel <ruiemanuelvieira@usp.br>
2020-05-24 04:39:13 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,157 @@
+#include <stdio.h>
+//Problema que eu percebi: Não dá pra mover o O, P e Q mesmo depois que as casas ao redor tão livres
+
+int temdiagonal(int i, int j){ //Verifica se a casa atual permite movimentos diagonais
+	if (i == 0 || i == 2 || i == 4 || i == 6){ 
+		if (j % 2 == 0){ //Em casas de linha par (começando a partir de zero), colunas pares permitem movimento diagonal
+			return 1; //Permite movimento diagonal
+		}
+		return -1; //Não permite movimento diagonal
+	}
+	else{ 
+		if (j % 2 != 0){ //Em casas de linha ímpar, colunas ímpares permitem movimento diagonal 
+			return 1; //Permite movimento diagonal
+		}
+		return -1; //Não permite movimento diagonal
+	}
+}
+
+void imprimir(char tab[7][7]){ //Imprime o tabuleiro de uma forma agradável ao usuário
+	int i, j;
+	for (i= 0; i < 7; i++){ //Loop para cada linha
+		printf("(%d)  ", i+1); //Imprime a linha atual
+		for (j = 0; j < 7; j++){ 
+				printf("%c", tab[i][j]);
+				if (j != 6 && tab[i][j] != ' ' && tab[i][j+1] != ' '){
+					printf("--");
+				}
+				else{
+					printf("  ");
+				}
+			}
+		if (i == 1 || i == 5){ 
+			printf("\n           | /|\\ |"); 
+		}
+		if (i == 0 || i == 4){ 
+			printf("\n           | \\|/ |"); 
+		}
+		if (i == 2){
+			printf("\n     | \\|/ | \\|/ | \\|/ | ");
+		}
+    if (i == 3) {
+      printf("\n     | /|\\ | /|\\ | /|\\ | \n");
+    }
+		else{ 
+			printf("\n");
+		}
+		}
+	printf("    (1)(2)(3)(4)(5)(6)(7)\n\n"); //Imprime as colunas
+}
+
+int casaehvalida(char casa){ //Verifica se a casa do movimento selecionado está livre e faz parte do tabuleiro
+	if (casa == 'o'){ 
+		return 1;
+	}
+	return -1;
+}
+
+int casa_adjacente (int i, int j, int x, int y) { //verfica se as coordenadas do movimento são adjacentes a casa atual do personagem, i e j são as coordenadas do movimento, e x e y são as coordenadas atuais do personagem
+    
+		if (temdiagonal(x, y) == 1){ 
+			if ((i == x + 1 || i == x || i == x-1) && (j == y + 1  || j == y -1 || j == y)){ //Caso a casa do personagem tenha diagonal, se as coordenadas colocadas pelo usuário forem antecessoras ou sucessoras da coordenada do personagem, ela será adjacente
+				return 1; //Casa é adjacente
+			}
+			return -1; //Casa não é adjacente
+		}
+		else{
+			if((i == x + 1 && j == y) || (i == x -1 && j == y) || (i == x && j == y - 1) || (i == x && j == y + 1)){ //Se a casa do personagem não tiver diagonal, haverá apenas 4 casos de casas adjacentes:A casa pode estar abaixo, na qual a coordenada da linha será +1 e a da coluna será igual/A casa pode estar acima, na qual a coordenada da linha será -1 e a da coluna será igual/ A casa pode estar à esquerda, que no caso a coordenada da linha seria igual e a coordenada da coluna será -1/A casa pode estar à direita, na qual a coordenada da linha será igual e a coordenada da coluna será +1
+				return 1; //Casa é adjacente
+			}
+			return -1; //Casa não é adjacente
+		}
+  }
+
+
+int procurarletra(char letra, int *a, int *b, char m[7][7]){ //Verifica se a letra digitada é um personagem e armazena as posições dele
+	int i, j; 
+	if (letra == ' ' || letra == 'o'){ //Não é um personagem
+		return -1;
+	}
+	for (i = 0; i < 7; i++){ 
+		for (j = 0; j < 7; j++){
+			if (m[i][j] == letra) { //Verifica se existe a letra em todas as casas do tabuleiro
+				*a = i; //Armazena a linha
+				*b = j; //Armazena a coluna
+				return 1;
+			}
+		}
+}
+	return -1; //Se tiver chegado até aqui, não pertence ao tabuleiro
+}
+
+int entrecasas(int i, int j, int x, int y, char m[7][7], int *a, int *b){ //i e j são as coordenadas do movimento da raposa, x e y são as coordenadas atuais da raposa e a e b são as coordenadas do ganso entre a raposa e a casa do movimento (se houver)
+*a = (i + x)/2; 
+*b = (j + y)/2; 
+//As variáveis acima fazem a conta do ponto médio entre a casa atual da raposa e a casa para a qual ela deve se mover, calculando assim as coordenadas da casa que ela realiza o movimento de "comer" o ganso
+if (casa_adjacente(i, j, x, y) == 1){ //Se o movimento for adjacente, não precisa verificar se dá pra comer, porque é um movimento normal
+	return 0;
+}
+if (m[*a][*b] == 'o' || m[*a][*b] == ' '){ 
+	return -1; //A entrecasa não é um ganso
+}
+if (casa_adjacente (x, y, *a, *b) == -1){ //O ganso digitado não está adjacente da raposa
+	return -1;
+}
+return 1; //Dá pra comer um ganso
+}
+
+int movimentovalido(char m[7][7], int x, int y){ 
+	int i, j;
+	for (i = -1; i <= 1; i++){ 
+		for (j = -1; j <= 1; j++){ //Analisa todas as casas da matriz
+			if (casaehvalida(m[x+i][x+j]) == 1){ //Verifica se a casa adjacente está livre
+				return 1;
+				}
+			}
+		}
+	return -1; //Se a função tiver passado pelos loops
+}
+
+int main(void) {
+	int i, j, a, b, c, d, g;
+	g = 0; //g é o número de gansos que já foram comidos
+  char ganso;
+  char m[7][7] = {{"  ooo  "}, {"  ooo  "}, {"AooZooK"}, {"BoooooJ"}, {"CDEFGHI"}, {"  LMN  "}, {"  OPQ  "}}; //Configuração Inicial
+	imprimir(m);	
+	  while(g <= 13){ //Pois, como há 17 anos, e precisam sobrar 4 ou menos para a raposa ganhar, 
+			do {//Jogada da raposa
+	        printf("É a vez da raposa, digite as coordenadas da casa para qual deseja se movimentar: ");
+	        scanf("%d %d", &i, &j);
+					procurarletra('Z', &a, &b, m);
+	    } while (casaehvalida(m[i-1][j-1]) == -1 || (casa_adjacente(i-1, j-1, a, b) == -1 && entrecasas(i-1, j-1, a, b, m, &c, &d) < 0)); //Caso a casa digitada não esteja livre ou não é adjacente à raposa
+        m[i-1][j-1] = 'Z'; //Movimenta a raposa
+				m[a][b] = 'o'; //Limpa a a casa que ela estava
+				if (entrecasas(i-1, j-1, a, b, m, &c, &d) == 1){
+					m[c][d] = 'o';
+					g++;
+				}
+			system("clear");
+	    imprimir(m); 
+	    
+			do {
+            printf("Digite o ganso que você deseja movimentar: ");
+            scanf("%c", &ganso);
+	    } while (procurarletra(ganso, &a, &b, m) == -1 || movimentovalido(m, a, b) == -1); //Caso o ganso digitado não esteja no tabuleiro ou o movimento não seja válido
+	    do {
+            printf("Agora digite as coordenadas da casa para qual deseja se movimentar: ");
+            scanf("%d %d", &i, &j);
+						procurarletra(ganso, &a, &b, m);
+        } while (casaehvalida(m[i-1][j-1]) == -1 || casa_adjacente(i-1, j-1, a, b) == -1); //Caso a casa digitada não esteja livre ou não seja adjacente ao ganso
+        system ("clear");
+				m[a][b] = 'o';
+				m[i-1][j-1] = ganso;
+        imprimir(m);
+		}
+	printf("Você ganhou o jogo!");
+  return 0;
+}
